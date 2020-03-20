03f0ccddf29de34bece28c24f6f6052e21f24c70
ramdevoxx <madim_raouf@yahoo.fr>
2020-03-20 13:39:28 +0100

    spring Admin Configuration

--- src/main/java/com/devox/configuration/PropertiesConfiguration.java
+++ src/main/java/com/devox/configuration/PropertiesConfiguration.java
@@ -1,9 +1,11 @@
 package com.devox.configuration;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
+@ConfigurationProperties(prefix="dynamique")
 @PropertySource("classpath:reload/application-reload-configuration.properties")
 @PropertySource("classpath:static/application-dev.properties")
 public class PropertiesConfiguration {
--- src/main/java/com/devox/reactive/ReactiveApplication.java
+++ src/main/java/com/devox/reactive/ReactiveApplication.java
@@ -1,15 +1,18 @@
 package com.devox.reactive;
 
+import com.devox.configuration.PropertiesConfiguration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAdminServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.devox")
-//@EnableConfigServer
+@EnableConfigurationProperties(PropertiesConfiguration.class)
 public class ReactiveApplication {
 
     public static void main(String[] args) {
