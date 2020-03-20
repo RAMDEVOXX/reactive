2a4ce7e90833becd7a86d71b5e37a2ddbdc48ca5
ramdevoxx <madim_raouf@yahoo.fr>
2020-03-20 14:23:59 +0100

    TEST HOT RELOAD

--- src/main/java/com/devox/controller/HomeController.java
+++ src/main/java/com/devox/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.devox.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableAutoConfiguration
 @RestController
 @RefreshScope
 public class HomeController {
