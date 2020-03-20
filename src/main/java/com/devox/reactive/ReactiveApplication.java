03abbe59f6ea753301c7361f66b341acf0692365
ramdevoxx <madim_raouf@yahoo.fr>
2020-03-20 03:06:18 +0100

    spring Admin Configuration

--- src/main/java/com/devox/reactive/ReactiveApplication.java
+++ src/main/java/com/devox/reactive/ReactiveApplication.java
@@ -1,11 +1,15 @@
 package com.devox.reactive;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableAdminServer
+//@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.devox")
+//@EnableConfigServer
 public class ReactiveApplication {
 
     public static void main(String[] args) {
